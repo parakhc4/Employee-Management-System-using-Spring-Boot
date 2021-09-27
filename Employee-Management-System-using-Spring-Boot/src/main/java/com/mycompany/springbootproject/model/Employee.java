fdb51e2e38419a7b54864c5ccba253a79f23eb53
Niloy Chakravorty <chakravortyniloy@gmail.com>
2021-09-28 00:42:52 +0530

    Documentation

--- Employee-Management-System-using-Spring-Boot/src/main/java/com/mycompany/springbootproject/model/Employee.java
+++ Employee-Management-System-using-Spring-Boot/src/main/java/com/mycompany/springbootproject/model/Employee.java
@@ -11,31 +11,31 @@ import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.List;
 
-@Data
-@NoArgsConstructor
-@Entity
+@Data // generates all the boilerplate that is normally associated with simple POJOs
+@NoArgsConstructor // will generate a constructor with no parameters
+@Entity // annotation specifies that the class is an entity and is mapped to a database table
 @Table(name = "empTable") // specifies the name of the database table to be used for mapping.
 public class Employee {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id // annotation specifies the primary key of an entity
+    @GeneratedValue(strategy = GenerationType.AUTO) // annotation provides for the specification of generation strategies for the values of primary keys
     private Integer id;
-    @Size(min = 2, max = 20)
-    @NotNull(message = "Pls Enter First Name!")
+    @Size(min = 2, max = 20) // specifies size of the variable
+    @NotNull(message = "Pls Enter First Name!") // does not permit to pass a null value
     private String firstName;
-    @Size(min = 2, max = 20)
-    @NotNull(message = "Pls Enter Last Name!")
+    @Size(min = 2, max = 20) // specifies the size of the variable
+    @NotNull(message = "Pls Enter Last Name!") // does not permit to pass a null value
     private String lastName;
 
-    @NotNull(message = "Pls Enter Email")
-    @Email
+    @NotNull(message = "Pls Enter Email") // does not permit to pass a null value
+    @Email // annotations to validate email address sent to a RESTful Web Service in HTTP GET Request
     private String email;
-    @NotNull
+    @NotNull // does not permit to pass a null value
     private LocalDate dob;
     //Employee Email
 
 
-    @ManyToOne(targetEntity = Department.class, cascade = CascadeType.ALL)
-    @JoinColumn(name = "deptTable", referencedColumnName = "departId")
+    @ManyToOne(targetEntity = Department.class, cascade = CascadeType.ALL) // annotations define many to one relation
+    @JoinColumn(name = "deptTable", referencedColumnName = "departId") // marks a column for as a join column for an entity association or an element collection.
     private Department department;
 
 }
